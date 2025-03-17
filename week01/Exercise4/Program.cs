aeea24f2647e085e7670fe52d8d0d4c0ce7f135a
Brandon <beechimuti@gmail.com>
2025-03-17 22:56:08 +0200

    Added code

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userNumber;
+
+        do
+        {
+            Console.WriteLine("Enter a number (0 to quit): ");
+            string userResponce = Console.ReadLine();
+
+            if (int.TryParse(userResponce, out userNumber) && userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+            else if (!int.TryParse(userResponce, out _))
+            {
+                Console.WriteLine("invalid input. Please enter valid number.");
+            }
+
+        } while (userNumber !=0);
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            float average = (float)numbers.Average();
+            int max = numbers.Max();
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The max is: {max}");
+        }
+        else   
+        {
+            Console.WriteLine("No numbers were entred.");
+        }
     }
 }
