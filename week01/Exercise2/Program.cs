abf9198cd5afdf13547429ea051e812743c243e7
Brandon <beechimuti@gmail.com>
2025-03-17 15:08:39 +0200

    created code

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage");
+        if (int.TryParse(Console.ReadLine(), out int percent))
+        {
+            String letter = percent switch 
+            {
+                >= 90 => "A",
+                >= 80 => "B",
+                >= 70 => "C",
+                >= 60 => "D",
+                _ => "F"
+            };
+            Console.WriteLine($"Your grade id: {letter}");
+
+            if (percent >= 70)
+            Console.WriteLine("You passed");
+        }
+        else
+        {
+            Console.WriteLine("invalid input. Please enter a valid Number.");
+        }
     }
 }
