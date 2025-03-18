3ee8fb79d4fa26415b685f7f58ddedd5548561e5
Brandon <beechimuti@gmail.com>
2025-03-18 14:58:11 +0200

    made changes to the code

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string name = PromptUserInput("Please enter your name");
+        int number = PromptNumberImput("Please enetr your favorite number: ");
+
+        int squared = CalculateSquare(number);
+        ShowResult(name, squared);
     }
+    static void DisplayWelcomeMessage() => Console.WriteLine("Welcome to the program");
+
+    static string PromptUserInput(string message)
+    {
+        Console.Write(message);
+        return Console.ReadLine();
+    }
+
+    static int PromptNumberImput(string message)
+    {
+        Console.Write(message);
+        return int.TryParse(Console.ReadLine(), out int num)? num : 0;
+    }
+
+    static int CalculateSquare(int num) => num * num;
+
+    static void ShowResult(string name, int squared) =>
+        Console.WriteLine($"{name}, the square of your number is {squared}");
 }
